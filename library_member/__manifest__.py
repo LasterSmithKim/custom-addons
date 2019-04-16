42be11b7cd7a8caadf7cad422268507b0645a3f5
LasterSmithKim <kingone@yeah.net>
2019-04-16 22:52:13 +0800

    Initial commit

---
+++ library_member/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Library Members',
+    'description': 'Manage people who will be able to borrow books.',
+    'author': 'SmithKim',
+    'depends': ['library_app'],
+    'application': False,
+    'data': [
+        'views/book_view.xml',
+    ],
+}
