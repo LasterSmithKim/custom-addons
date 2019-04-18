41d5daca89471593dde84ebe62eb0103e27b10de
LasterSmithKim <kingone@yeah.net>
2019-04-18 23:36:33 +0800

    重新理解xml结构 对record标签的理解重新认识

--- library_member/__manifest__.py
+++ library_member/__manifest__.py
@@ -2,9 +2,14 @@
     'name': 'Library Members',
     'description': 'Manage people who will be able to borrow books.',
     'author': 'SmithKim',
-    'depends': ['library_app'],
+    'depends': ['library_app','mail'],
     'application': False,
     'data': [
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
         'views/book_view.xml',
+        'views/library_menu.xml',
+        'views/member_view.xml',
+        'views/book_list_template.xml',
     ],
 }
