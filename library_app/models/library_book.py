f8a0c1beb6af5b8cf6a0210c3409412b21cd050c
LasterSmithKim <kingone@yeah.net>
2019-06-01 21:10:52 +0800

    library_checkout 新模块

--- library_app/models/library_book.py
+++ library_app/models/library_book.py
@@ -4,13 +4,57 @@ from odoo.exceptions import Warning
 class Book(models.Model):
     _name = 'library.book'
     _description = 'Book'
-    name = fields.Char('Title', required=True)
+    _order = 'date_published,name desc'
+
+    # String fields
+    name = fields.Char(
+        'Title',
+        default=None,
+        index=True,
+        help='Book cover title',
+        readonly=False,
+        required=True,
+        translate=False,
+    )
     isbn = fields.Char('ISBN')
-    active = fields.Boolean('Active?', default=True)
+    book_type = fields.Selection(
+        [('paper', 'Paperback'),
+         ('hard', 'Hardcover'),
+         ('electronic', 'Electronic'),
+         ('other', 'Other')],
+        'Type')
+    notes = fields.Text('Internal Notes')
+    descr = fields.Html('Description')
+
+    # Numeric fields:
+    copies = fields.Integer(default=1)
+    avg_rating = fields.Float('Average Rating', (3, 2))
+    price = fields.Monetary('Price', 'currency_id')
+    currency_id = fields.Many2one('res.currency')  # price helper
+
+    # Date and time fields
     date_published = fields.Date()
+    last_borrow_date = fields.Datetime(
+        'Last Borrowed On',
+        default=lambda self: fields.Datetime.now(),
+    )
+
+    # Other fields
+    active = fields.Boolean('Active?', default=True)
     image = fields.Binary('Cover')
-    publisher_id = fields.Many2one('res.partner', string='Publisher')
+
+    # Relational Fields
+    publisher_id = fields.Many2one(
+        'res.partner', string='Publisher')
     author_ids = fields.Many2many('res.partner', string='Authors')
+    publisher_country_id = fields.Many2one(
+        'res.country', string='Publisher Country',
+        #related='publisher_id.country_id',
+        compute='_compute_publisher_country',
+        # store = False, @ 默认不在数据库中存储
+        # inverse='_inverse_publisher_country',
+        # search='_search_publisher_country',
+    )
 
     @api.multi
     def _check_isbn(self):
@@ -32,3 +76,8 @@ class Book(models.Model):
             if book.isbn and not book._check_isbn():
                 raise Warning('%s is an invalid ISBN' % book.isbn)
             return True
+
+    @api.depends('publisher_id.country_id')
+    def _compute_publisher_country(self):
+        for book in self:
+            book.publisher_country_id = book.publisher_id.country_id
