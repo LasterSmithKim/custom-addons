4a868fa97fdc4634fbad5f2369bcb8c898554e40
LasterSmithKim <kingone@yeah.net>
2019-06-13 17:06:15 +0800

    web模块

---
+++ library_website/controllers/main.py
@@ -0,0 +1,12 @@
+from odoo import http
+from odoo.http import request
+
+
+class Main(http.Controller):
+    @http.route('/checkouts', auth='user', website=True)
+    def checkouts(self, **kwargs):
+        Checkout = request.env['library.checkout']
+        checkouts = Checkout.search([])
+        return request.render(
+            'library_website.index',
+            {'docs': checkouts})
